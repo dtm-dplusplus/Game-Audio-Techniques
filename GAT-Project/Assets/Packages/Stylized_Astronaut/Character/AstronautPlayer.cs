51d733247b150d4bea0234fbe61ae3d7336057d7
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2024-01-16 17:37:04 +0000

    Spaceship flying + SFX

---
+++ GAT-Project/Assets/Packages/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a892e7eb8ccee08e5ad0fbacb82919035f407273a401351bc3cdf09db780ba26
+size 948
---
+++ GAT-Project/Assets/Packages/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2bf402064c626a481255c472afe4d4747734f143027199bfc9287655e09f1b0c
+size 1078
